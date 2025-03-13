a17b9fb86abdfa1327b564fe0bc6195f0e52dacd
Mukho <mukho@kakao.com>
2025-03-13 14:36:39 +0900

    second commit

--- src/main/java/com/mukho/stomp/controller/StompController.java
+++ src/main/java/com/mukho/stomp/controller/StompController.java
@@ -1,14 +1,24 @@
 package com.mukho.stomp.controller;
 
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Controller;
 
 import com.mukho.stomp.dto.ReqDto;
@@ -16,6 +26,7 @@ import com.mukho.stomp.dto.ResDto;
 import com.mukho.stomp.dto.ResSessionsDto;
 import com.mukho.stomp.listener.StompEventListener;
 
+import jakarta.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -26,10 +37,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StompController {
 
+	private final TaskScheduler taskScheduler;
+
 	private final StompEventListener eventListener;
 
-	public StompController(StompEventListener eventListener) {
+	private final SimpMessagingTemplate	messagingTemplate;
+
+	private final ConcurrentHashMap<String, ScheduledFuture<?>> sessionMap = new ConcurrentHashMap<>();
+
+	public StompController(TaskScheduler taskScheduler, StompEventListener eventListener, SimpMessagingTemplate messagingTemplate) {
+		this.taskScheduler = taskScheduler;
 		this.eventListener = eventListener;
+		this.messagingTemplate = messagingTemplate;
 	}
 
 	@MessageMapping("/hello") // 수신 /app/hello
@@ -62,5 +81,81 @@ public class StompController {
 		Set<String> sessions = eventListener.getSessions();
 
 		return new ResSessionsDto(sessions.size(), sessions.stream().toList(), sessionId, LocalDateTime.now());
+	}
+
+	// Programmatically Send
+	@MessageMapping("/code1") // /app/code1
+	public void code1(ReqDto reqDto, Message<ReqDto> message, MessageHeaders headers) {
+		log.info("reqDto : {}", reqDto);
+		log.info("message : {}", message);
+		log.info("headers : {}", headers);
+
+		ResDto resDto = new ResDto(reqDto.getMessage().toUpperCase(), LocalDateTime.now());
+		messagingTemplate.convertAndSend("/topic/hello", resDto);
+	}
+
+	@MessageMapping("/code2") // /app/code2
+	public void code2(ReqDto reqDto, MessageHeaders headers) {
+		log.info("reqDto : {}", reqDto);
+		String sessionId = headers.get("simpleSessionId").toString();
+		log.info("simpleSessionId : {}", sessionId);
+
+		Set<String> sessions = eventListener.getSessions();
+		ResSessionsDto resSessionsDto = new ResSessionsDto(sessions.size(), sessions.stream().toList(), sessionId, LocalDateTime.now());
+
+		messagingTemplate.convertAndSendToUser(sessionId, "/queue/sessions", resSessionsDto, createHeaders(sessionId));
+	}
+
+	MessageHeaders createHeaders(@Nullable String sessionId) {
+		SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
+		headerAccessor.setSessionId(sessionId);
+		headerAccessor.setLeaveMutable(true);
+		return headerAccessor.getMessageHeaders();
+	}
+
+	@MessageMapping("/start") // /app/start
+	public void start(ReqDto reqDto, MessageHeaders headers) {
+		log.info("reqDto : {}", reqDto);
+		String sessionId = headers.get("simpleSessionId").toString();
+		log.info("simpleSessionId : {}", sessionId);
+
+		ScheduledFuture<?> scheduleFuture = taskScheduler.scheduleAtFixedRate(() -> {
+			Random random = new Random();
+			int currentPrice = random.nextInt(100);
+			messagingTemplate.convertAndSendToUser(sessionId, "/queue/sessions", currentPrice, createHeaders(sessionId));
+		}, Duration.ofSeconds(3));
+		sessionMap.put(sessionId, scheduleFuture);
+
+		// Thread.sleep(10000);
+		// scheduledFuture.cancel(true);
+	}
+
+	@MessageMapping("/stop") // /app/stop
+	public void stop(ReqDto reqDto, MessageHeaders headers) {
+		log.info("reqDto : {}", reqDto);
+		String sessionId = headers.get("simpleSessionId").toString();
+		log.info("simpleSessionId : {}", sessionId);
+
+		ScheduledFuture<?> remove = sessionMap.remove(sessionId);
+		remove.cancel(true);
+	}
+
+	@MessageMapping("/exception")
+	@SendTo("/topic/hello")
+	public void exception(ReqDto request, MessageHeaders headers) throws Exception {
+		log.info("request: {}", request);
+		String message = request.getMessage();
+		switch(message) {
+			case "runtime":
+				throw new RuntimeException();
+			case "nullPointer":
+				throw new NullPointerException();
+			case "io":
+				throw new IOException();
+			case "exception":
+				throw new Exception();
+			default:
+				throw new InvalidParameterException();
+		}
 	}
 }
